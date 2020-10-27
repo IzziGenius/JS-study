c78d29ba1f37edb8e8f4e4a1de220eb1fd2a1093
IzziGenius <git config --global user.email vpupkin@company.com>
2020-10-27 20:44:27 +0300

    ot work lesson15

--- js/Java_dz.js
+++ js/Java_dz.js
@@ -10,15 +10,30 @@ actors:{},
 genres:[],
 privat: false
 
-};
+}; 
 
+
+for(let i=0; i<2; i++){
 const ask1= prompt("Один из последних просмотренных фильмов?", ""),
- ask1$2=prompt("На сколько оцените его?", ""),
- ask2= prompt("Один из последних просмотренных фильмов?", ""),
- ask2$2=prompt("На сколько оцените его?", "");
+      ask2=prompt("На сколько оцените его?", "");
+     
+     if(ask1 !="" && ask2 !="" && ask1.length <50 && ask2.length <50 && ask1 !=null && ask2 !=null) 
+{personalMovieDB.movies[ask1] = ask2;}
+else {i--;}
 
-personalMovieDB.movies[ask1] = ask1$2;
-personalMovieDB.movies[ask2] = ask2$2;
+}
+
+if(personalMovieDB.count<10){
+    alert("Просмотренно довольно мало фильмов");
+}else {
+    if(personalMovieDB.count>=10 && personalMovieDB.count<30 ){
+    alert("Вы классический зритель");
+} else if(personalMovieDB.count>=30){
+    alert("Вы киноман");
+} else{
+        alert("Произошла ошибка");}
+    }
+
 
 
 console.log(personalMovieDB);
